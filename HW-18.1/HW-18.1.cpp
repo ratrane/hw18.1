741f5b4eca3e50184b1dba816ab34b1001636ec8
boosh <boo6ik@outlook.com>
2021-02-07 13:58:24 +0300

    Enter coments for every action

--- HW-18.1/HW-18.1.cpp
+++ HW-18.1/HW-18.1.cpp
@@ -8,54 +8,51 @@ using namespace std;
 
 class Stack
 {
-private:
-
-	int value;
 
 public:
-
-	int count = 0;
-	int ArraySize;
-	int* Array = new int[ArraySize];
+										
+	int count = 0;							//Create count for elements in stack
+	int ArraySize;							//and size of array
+	int* Array = new int[ArraySize];		//Create new array, which is a pointer to first array
 
 	Stack()
 	{
 		cout<< "Enter size of array: ";
-		cin >> ArraySize;
+		cin >> ArraySize;					//Enter stack size
 	}
 
-	void push()
+	void push()								//Func, that push some int in stack
 	{
-		count++;
+		count++;							//Plus one step to count. Must not to exceed ArraySize
 		cout << "Enter value for push: ";
-		cin >> Array[count];
+		cin >> Array[count];				//Enter value for int, which will be pushed in array
 		cout << Array[count] << " pushed" << endl;
 	};
 
-	void pop()
+	void pop()								//Func, that shows last value from array and remove it
 	{	
 		cout << Array[count] << " removed" << endl;
-		count--;
+		count--;							// Minus one step to count
 	}
 };
 
 int main()
 {
-	Stack* temp = new Stack;	
-	int option;
-	int k = 1;
+	Stack* temp = new Stack;				//Call class Stack and create pointer temp for it
+	int option;								//Create new int, that will used in menu in console
+	int k = 1;								//Default option for menu
 
 	while (k == 1)
 	{
-		cout << "Options: 0 - Exit, 1 - push value, 2 - pop value" << endl<< "Enter option: ";
+		cout << "Options: 0 - Exit, 1 - push value, 2 - pop value" << endl << "Enter option: "; //Enter value for call function
 		cin >> option;
 
 		switch (option)
 		{
 		case 0:
-				k = 0;
-				break;
-			
+			k = 0;
+			break;
+
 		case 1:
 			if ((temp->ArraySize) > (temp->count))
 			{
@@ -63,15 +60,16 @@ int main()
 			}
 			else
 			{
-				cout << "Stack is full"<<endl;
+				cout << "Stack is full" << endl;
 			}
 			break;
-		
+
 		case 2:
 			temp->pop();
-		
+
+		}
 	}
-	
 }
+
 
 
